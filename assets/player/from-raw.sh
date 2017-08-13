3ed69fac3722d6d48b95b3d9be41cce82361f84d
Matt Edlefsen <matt.edlefsen@gmail.com>
2017-08-12 23:37:39 -0400

    Add player animations

---
+++ assets/player/from-raw.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+convert raw/transition/player_transition1.png -scale 182x200 stationary.png
---
+++ assets/player/strike_left/from-raw.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+files=( `printf "%s\n" ../raw/transition/*.png | sort` `printf "%s\n" ../raw/*.png | sort | tail -n +2` )
+i=1
+for f in "${files[@]}"
+do
+  convert "$f" -scale 182x200 -flop "$(printf "%04d.png" $i)"
+  let i++
+done
