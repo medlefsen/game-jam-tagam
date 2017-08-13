1a9b7088779f243af2b7a2a41bd3c38d66c0dbda
Matt Edlefsen <matt.edlefsen@gmail.com>
2017-08-13 13:52:47 -0400

    Change page title and add favicon

--- assets/player/from-raw.sh
+++ assets/player/from-raw.sh
@@ -2,3 +2,4 @@
 cd "$(dirname "$0")"
 
 convert raw/transition/player_transition1.png -scale 182x200 stationary.png
+convert raw/transition/player_transition1.png -scale 64x64 -extent 64x64 -gravity center -background none favicon.ico
